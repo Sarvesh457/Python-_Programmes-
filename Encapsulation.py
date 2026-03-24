721d93932a06a7f63799bfdc6f51c61c9a0dab71
Sarvesh Mahajan <sarveshmahajan457@gmail.com>
2026-03-24 22:31:52 +0530

    Add files via upload

---
+++ Encapsulation.py
@@ -0,0 +1,26 @@
+class Arithmatic:
+    def __init__(self,A,B):
+        self.No1 = A
+        self.No2 = B
+        print("Object gets created Succesfully")
+
+    def Addition(self):
+        Ans = 0
+        Ans = self.No1+self.No2
+        return Ans
+    
+    def Substraction(self):
+        Ans = 0
+        Ans = self.No1-self.No2
+        return Ans
+
+obj1 = Arithmatic(11,10)            # Arithmatic(id(obj1)11,10) -> __init__(id(obj1)11,10)
+obj2 = Arithmatic(21,20)            # Arithmatic(id(obj2)21,20) -> __init__(id(obj2)21,20)
+
+Ret = obj1.Addition()               # Addition(id(obj1)) -> Addition(1000)
+
+print(Ret)
+
+Ret = obj2.Substraction()           # Substraction(id(obj2)) -> Substraction(2000)
+
+print(Ret)
---
+++ ExceptionDemo3.py
@@ -0,0 +1,28 @@
+def main():
+    Ans = 0
+    
+    try :
+        print("Inside try")
+        
+        print("Enter first number :")
+        No1 = int(input())
+        print("Enter Second number :")
+        No2 = int(input())
+        
+        Ans = No1/No2
+    except ZeroDivisionError as zobj:
+        print("Inside except :",zobj)
+    
+    except ValueError as vobj :
+        print("Inside except :",vobj)
+    
+    except Exception as eobj :                                  # main root class of exception
+        print("Inside except : ",eobj)
+    
+    finally : 
+        print("Inside Finally")
+
+    print("Division is ",Ans)
+
+if __name__ == "__main__":
+    main()
