f4cf23c57643d3c21a0b7d81b6059cf65a383b55
Sarvesh Mahajan <sarveshmahajan457@gmail.com>
2026-03-26 21:47:33 +0530

    Add files via upload

---
+++ Function_2.py
@@ -0,0 +1,9 @@
+print("Enter First Number : ")
+No1 = input()
+
+print("Enter Second Number : ")
+No2 = input()
+
+Ans = No1*No2                           # not good ....
+
+print("Multiplication is : ",Ans)
