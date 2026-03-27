a17902018da5137817e6ac9bfb8e9bf81882442b
Sarvesh Mahajan <sarveshmahajan457@gmail.com>
2026-03-27 23:15:49 +0530

    Add files via upload

---
+++ FunctionDemo5.py
@@ -0,0 +1,12 @@
+# Accept : One Parameter
+# Return : Nothing
+
+def Marvellous1(Value):
+    print("Inside Marvellous 1 :",Value)
+
+def main():
+    Marvellous1("Python")
+    Marvellous1(51)
+
+if __name__ == "__main__":
+    main()
