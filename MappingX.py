a7fa2a7cce51057ff09c657cc4c6b1ae61420c05
Sarvesh Mahajan <sarveshmahajan457@gmail.com>
2026-04-16 11:38:42 +0530

    Add files via upload

---
+++ MappingX.py
@@ -0,0 +1,8 @@
+Information = {"Name":"Rahul","Age":25,"City":"Pune","Marks":89.90,"City" : "Mumbai"}     # Ordered
+
+print(Information)
+
+print(Information["City"])              # Mumbai over write the pune
+Information["Age"] = 26                 # Mutable Value
+
+print(Information)
